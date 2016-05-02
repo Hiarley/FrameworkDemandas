74bb8fea4e51c0aa044a78007c24c2c02a908592
Hiarley Farias <Hiarley Farias>
2016-05-02 01:31:59 -0300

    Implementando classe Setor.

--- src/domain/Setor.java
+++ src/domain/Setor.java
@@ -10,5 +10,59 @@ package domain;
  * @author hiarl
  */
 public class Setor {
+    private long id;
+    private String nome;
+    private String descricao;
+
+    public Setor() {
+    }
+
+    public Setor(long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
     
 }
