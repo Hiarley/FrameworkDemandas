0aa8b7283317dd58addf1a86f8ee820888c08a30
Thiagolink <t.t.2007@hotmail.com>
2016-05-02 14:33:47 -0300

    classe notificacao parametros e get e set

--- src/domain/NotificaWhatsapp.java
+++ src/domain/NotificaWhatsapp.java
@@ -11,4 +11,6 @@ package domain;
  */
 public class NotificaWhatsapp extends Notificao{
     
+    
+    
 }
--- src/domain/Notificao.java
+++ src/domain/Notificao.java
@@ -10,5 +10,31 @@ package domain;
  * @author hiarl
  */
 public abstract class Notificao {
+    private String mensagem;
     
+    
+    public Notificao() {
+    }
+
+    public Notificao(String mensagem) {
+        this.mensagem = mensagem;
+    }
+    
+    
+
+    /**
+     * @return the mensagem
+     */
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    /**
+     * @param mensagem the mensagem to set
+     */
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+    
+    //public abstract void enviar();
 }
