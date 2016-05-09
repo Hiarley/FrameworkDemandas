5616c1a696fa2e470a3afd114f0d4d395abe373a
Thiagolink <t.t.2007@hotmail.com>
2016-05-09 19:45:59 -0300

    Adição de um pacote para interface gráfica e criação de interfaces para cada tipo de Usuário

---
+++ src/GUI/GUIAdministrador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author Thiago
+ */
+public interface GUIAdministrador {
+    public void cadastrarUsuario();
+    public void cadastrarInformacoes();
+    public void removerUsuario();
+    public void removerInformacoes();
+    public void listarUsuarios();
+    public void listarInformacoes();
+}
---
+++ src/GUI/GUIUsuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author Thiago
+ */
+public interface GUIUsuario {
+    public void cadastrarCliente();
+    public void removerCliente();
+    public void listarCliente();
+    public void analisarPedido();
+    
+}
