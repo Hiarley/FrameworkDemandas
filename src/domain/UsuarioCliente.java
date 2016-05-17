4cb337dd8973d6aa88651c3a5f2cdb1ba5f13700
Thiagolink <t.t.2007@hotmail.com>
2016-05-17 14:33:41 -0300

    adição de um arraylist de demandas em usuariocliente

--- src/domain/UsuarioCliente.java
+++ src/domain/UsuarioCliente.java
@@ -6,15 +6,41 @@
 package domain;
 
 import excecao.ClienteInvalidoException;
+import java.util.ArrayList;
 
 /**
  *
  * @author hiarl
  */
 public class UsuarioCliente extends Usuario{
+    
+    private ArrayList<Demanda> listaDemandas;
+
+    public UsuarioCliente() {
+    }
+
+    public UsuarioCliente(ArrayList<Demanda> listaDemandas, long id, String nome, String endereco, String telefone, String login, String senha) {
+        super(id, nome, endereco, telefone, login, senha);
+        this.listaDemandas = listaDemandas;
+    }
+    
      
     public  boolean validar() throws ClienteInvalidoException{
         return true;
+    }
+
+    /**
+     * @return the listaDemandas
+     */
+    public ArrayList<Demanda> getListaDemandas() {
+        return listaDemandas;
+    }
+
+    /**
+     * @param listaDemandas the listaDemandas to set
+     */
+    public void setListaDemandas(ArrayList<Demanda> listaDemandas) {
+        this.listaDemandas = listaDemandas;
     }
 
 }
