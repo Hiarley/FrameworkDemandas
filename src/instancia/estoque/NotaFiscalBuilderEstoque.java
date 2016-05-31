462ab0c1ae99c231c02501a4b553cc36b96261d0
Thiagolink <t.t.2007@hotmail.com>
2016-05-31 19:13:11 -0300

    Criação do builder para a nota fiscal

---
+++ src/instancia/estoque/NotaFiscalBuilderEstoque.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instancia.estoque;
+
+import domain.NotaFiscalBuilder;
+
+/**
+ *
+ * @author Thiago
+ */
+public class NotaFiscalBuilderEstoque implements NotaFiscalBuilder{
+    
+}
